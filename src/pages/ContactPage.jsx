3ff728c461048fd3cb3921265a332010daddc0a5
Tani1964 <ifegbesan6@gmail.com>
2026-08-13 15:38:59 +0100

    chore: remove collage

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -224,7 +224,7 @@ function ContactPage() {
                 <section className="rounded-lg border border-[#e6ebea] bg-[#f9faf9] p-4">
                   <div className="flex items-start gap-3">
                     <svg className="mt-0.5 h-5 w-5 shrink-0 text-[#d32f2f]" fill="currentColor" viewBox="0 0 24 24">
-                      <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2m0 18c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8m3.5-9c.83 0 1.5-.67 1.5-1.5S16.33 8 15.5 8 14 8.67 14 9.5s.67 1.5 1.5 1.5zm-7 0c.83 0 1.5-.67 1.5-1.5S9.33 8 8.5 8 7 8.67 7 9.5 7.67 11 8.5 11zm3.5 6.5c2.33 0 4.31-1.46 5.11-3.5H6.89c.8 2.04 2.78 3.5 5.11 3.5z" />
+                      <path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5a2.5 2.5 0 1 1 0-5 2.5 2.5 0 0 1 0 5z" />
                     </svg>
                     <div>
                       <p className="text-xs text-[#7f8890]">Office Address</p>
